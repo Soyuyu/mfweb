5f04aec62c4139eaa0e73c5a0de2e6c08a94a314
Martin Fowler <fowler@acm.org>
2013-07-03 13:27:19 -0400

    infodeck doc: added slides for code samples and builds

---
+++ sample/decks/infodeck/code/example.rb
@@ -0,0 +1,13 @@
+class Useless
+  # <codeFragment name = "all">
+  if sun_is_shining 
+    puts "hello sun"
+  end
+
+  # <codeFragment name = "meth">
+  def sun_is_shining
+    return 5 == (2 + 2)
+  end
+  # </codeFragment>
+  # </codeFragment>
+end

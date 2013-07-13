18d00ee54aaf514293b9b62c4c8b9f7b9cafe75d
Martin Fowler <fowler@acm.org>
2013-07-13 11:45:43 -0400

    infodeck: support for curved arrow

--- lib/mfweb/infodeck/arrowTr.rb
+++ lib/mfweb/infodeck/arrowTr.rb
@@ -1,20 +1,27 @@
 module Mfweb::InfoDeck
   class ArrowTransformer < DeckTransformer
+
+    def self.new html, anElement, maker
+      result = anElement['curve'] ? CurvedArrowTransformer.allocate : self.allocate
+      result.send :initialize, html, anElement, maker
+      return result
+    end
     
     def handle_arrow anElement
+      assert_valid_element
       svg_attrs, svg_style = {}, {}
       svg_attrs['width'] = "%dpx" % [svg_length]
       svg_attrs['height'] = "%dpx" % [svg_height]
       svg_attrs['class'] = 'arrow'
       svg_style['left'] = "%spx" % from_x
-      svg_style['top'] = "%spx" % (from_y - svg_height / 2.0)
+      svg_style['top'] = "%spx" % (from_y - svg_h_space)
       svg_style['transform-origin'] = "0 50%"
       svg_style['transform'] = "rotate(%1.2frad)" % path_angle
       populate_browser_prefixes svg_style, 'transform-origin', 'transform'
       add_to_style(svg_attrs, svg_style)
       @html.element("svg", svg_attrs) do
-        path_attrs = {'d' => [line_path, arrow_head_path].join(" ")}
-        @html.element("path", path_attrs)
+        @html.element("path", d: line_path)
+        emit_arrow_head
       end
     end
     def populate_browser_prefixes(attrs, *keys)
@@ -22,6 +29,19 @@ module Mfweb::InfoDeck
         %w[ms webkit moz o].each do |prefix|
           attrs["-%s-%s" % [prefix, k]] = attrs[k]
         end
+      end
+    end
+    def emit_arrow_head
+      @html.element("path", d: arrow_head_path)
+    end
+
+    def assert_valid_element
+      point_regex = %r{^\d+\s+\d+$}
+      if point_regex !~ @root['from']
+        raise "from is ill formatted <%s>" % @root['from']
+      end
+      if point_regex !~ @root['to']
+        raise "to is ill formatted <%s>" % @root['to']
       end
     end
 
@@ -52,11 +72,14 @@ module Mfweb::InfoDeck
     def head_dy
       head_dx * 0.6
     end
+    def svg_h_space
+      head_dx
+    end
     def svg_length 
       Math.sqrt((path_dx ** 2) + (path_dy ** 2))
     end
     def svg_height 
-      head_dx * 2
+      svg_h_space * 2
     end
     def path_length 
       svg_length - arrow_tip_offset
@@ -65,18 +88,65 @@ module Mfweb::InfoDeck
       Math.atan2(path_dy, path_dx)
     end
     def start_point 
-      "0,%d" % head_dx
+      Point.new(0,svg_h_space)
     end
     def end_point 
-      "%d,%f" % [path_length, 0]
+      Point.new(path_length, start_point.y)
     end
     def line_path 
-      "M %s l %s" % [start_point, end_point]
+      "M %s L %s" % [start_point, end_point]
     end
     def arrow_head_path 
-      "m %d,%d l %d,%d l %d,%d" % 
-        [-head_dx, -head_dy, head_dx, head_dy, -head_dx, head_dy]
+      "M %s m %d %d l %d %d l %d %d" % 
+        [end_point, -head_dx, -head_dy, head_dx, head_dy, -head_dx, head_dy]
     end
-    
+  end
+
+  class CurvedArrowTransformer < ArrowTransformer
+    def line_path 
+      "M %s Q %s, %s" % [start_point, control_point, end_point]
+    end
+    def curve
+      case @root['curve'] 
+      when 'left' then -0.2
+      when 'right' then 0.2
+      else "value <%s> unknown for curve" % @root['curve']
+      end
+    end
+    def control_offset 
+     path_length * curve
+    end
+    def control_point 
+      Point.new(path_length/2.0, start_point.y + control_offset)
+    end
+    def svg_h_space
+      [head_dx, control_offset.abs].max
+    end
+    def emit_arrow_head
+      attrs =  {d: arrow_head_path, 
+        transform: ("rotate(%d, %s)" % [arrow_angle, end_point]), }
+      @html.element("path", attrs)
+    end
+    def arrow_angle
+       90 * -curve # derived by trail and error
+    end
+    def degrees radians_value
+      radians_value * 180 / Math::PI
+    end
+   
+  end
+
+  class Point
+    def initialize x,y
+      @x = x
+      @y = y
+    end
+    attr_reader :x, :y
+    def to_s
+      "%1.2f,%1.2f" % [x,y]
+    end
+    def - arg
+      Point.new(x - arg.x, y - arg.y)
+    end
   end
 end
--- test/infodeck/arrowTrTester.rb
+++ test/infodeck/arrowTrTester.rb
@@ -9,19 +9,44 @@ module Mfweb::InfoDeck
 
     def transform aString
       @emitter = Mfweb::Core::HtmlEmitter.new
-      @btr = ArrowTransformer.new @emitter, Nokogiri::XML(aString).root, @maker
+      @btr = DeckTransformer.new @emitter, Nokogiri::XML(aString).root, @maker
       @btr.render      
       @html = Nokogiri::XML(@emitter.out)    
     end
 
-    def test_basic_arrow
-      transform %{<arrow from = "300 300" to = "100, 100" />}
+    def test_straight_arrow
+      transform %{<arrow from = "300 300" to = "100 100" />}
       expected = %{
-<svg class = 'arrow' height = '40px' style = '-moz-transform: rotate(-2.36rad);-moz-transform-origin: 0 50%;-ms-transform: rotate(-2.36rad);-ms-transform-origin: 0 50%;-o-transform: rotate(-2.36rad);-o-transform-origin: 0 50%;-webkit-transform: rotate(-2.36rad);-webkit-transform-origin: 0 50%;left: 300px;top: 280.0px;transform: rotate(-2.36rad);transform-origin: 0 50%;' width = '282px'>
-<path d = 'M 0,20 l 280,0.000000 m -20,-12 l 20,12 l -20,12'></path>
+<svg class = 'arrow' height = '40px' style = '-moz-transform: rotate(-2.36rad);-moz-transform-origin: 0 50%;-ms-transform: rotate(-2.36rad);-ms-transform-origin: 0 50%;-o-transform: rotate(-2.36rad);-o-transform-origin: 0 50%;-webkit-transform: rotate(-2.36rad);-webkit-transform-origin: 0 50%;left: 300px;top: 280px;transform: rotate(-2.36rad);transform-origin: 0 50%;' width = '282px'>
+<path d = 'M 0.00,20.00 L 280.84,20.00'></path>
+
+<path d = 'M 280.84,20.00 m -20 -12 l 20 12 l -20 12'></path>
 </svg>
 }
 
+      assert_equal expected, @emitter.out
+    end
+    def test_left_curve_arrow
+      transform %{<arrow from = "300 300" to = "100 100" curve = "left"/>}
+      expected = %{
+<svg class = 'arrow' height = '112px' style = '-moz-transform: rotate(-2.36rad);-moz-transform-origin: 0 50%;-ms-transform: rotate(-2.36rad);-ms-transform-origin: 0 50%;-o-transform: rotate(-2.36rad);-o-transform-origin: 0 50%;-webkit-transform: rotate(-2.36rad);-webkit-transform-origin: 0 50%;left: 300px;top: 243.8314575050762px;transform: rotate(-2.36rad);transform-origin: 0 50%;' width = '282px'>
+<path d = 'M 0.00,56.17 Q 140.42,0.00, 280.84,56.17'></path>
+
+<path d = 'M 280.84,56.17 m -20 -12 l 20 12 l -20 12' transform = 'rotate(18, 280.84,56.17)'></path>
+</svg>
+}
+
+      assert_equal expected, @emitter.out
+    end
+    def test_right_curve_arrow
+      transform %{<arrow from = "300 300" to = "100 100" curve = "right"/>}
+      expected = %{
+<svg class = 'arrow' height = '112px' style = '-moz-transform: rotate(-2.36rad);-moz-transform-origin: 0 50%;-ms-transform: rotate(-2.36rad);-ms-transform-origin: 0 50%;-o-transform: rotate(-2.36rad);-o-transform-origin: 0 50%;-webkit-transform: rotate(-2.36rad);-webkit-transform-origin: 0 50%;left: 300px;top: 243.8314575050762px;transform: rotate(-2.36rad);transform-origin: 0 50%;' width = '282px'>
+<path d = 'M 0.00,56.17 Q 140.42,112.34, 280.84,56.17'></path>
+
+<path d = 'M 280.84,56.17 m -20 -12 l 20 12 l -20 12' transform = 'rotate(-18, 280.84,56.17)'></path>
+</svg>
+}
 
       assert_equal expected, @emitter.out
     end

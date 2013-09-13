769b0e3e0dde27a34e9afb3b70151d2b13d8367a
Martin Fowler <fowler@acm.org>
2013-09-12 20:14:04 -0400

    core: author class more tolerant of missing info

--- lib/mfweb/core/author.rb
+++ lib/mfweb/core/author.rb
@@ -7,10 +7,13 @@ module Mfweb::Core
       @data.at_css('author-name').text
     end
     def url
-      @data.at_css('author-url').text
+      @data.at_css('author-url') ? @data.at_css('author-url').text : nil
     end
     def photo 
-      @data.at_css('author-photo')['src']
+      @data.at_css('author-photo')['src'] if has_photo?
+    end
+    def has_photo?
+      true == @data.at_css('author-photo')
     end
   end
 end
